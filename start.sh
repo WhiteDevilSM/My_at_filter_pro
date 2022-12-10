8b3055e21176ac46575ad56df7461832e92ed4f8
WHITE DEVIL - SM <105021702+WhiteDevilSM@users.noreply.github.com>
2022-12-10 23:20:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/WhiteDevilSM/My_at_filter_pro.git /RashmikaBot
+  git clone https://github.com/WhiteDevilSM/My_at_filter_pro.git /My_at_filter_pro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /RashmikaBot
+  git clone $UPSTREAM_REPO /My_at_filter_pro
 fi
-cd /RashmikaBot
+cd  /My_at_filter_pro
 pip3 install -U -r requirements.txt
 echo "Starting SM_Bot...."
 python3 bot.py
